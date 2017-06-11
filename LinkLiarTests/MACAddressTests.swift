03cb5be519352b1bda83270310ce874d611b8b8b
halo <github@posteo.org>
2017-06-11 22:07:48 +0200

    MACAddress conforms to Equatable

--- LinkLiarTests/MACAddressTests.swift
+++ LinkLiarTests/MACAddressTests.swift
@@ -5,22 +5,54 @@ class MACAddressTests: XCTestCase {
 
   func testFormattedWithAlreadyFormattedMAC() {
     let mac = MACAddress("aa:bb:cc:dd:ee:ff")
-    XCTAssertEqual("aa:bb:cc:dd:ee:ff", mac.formatted())
+    XCTAssertEqual("aa:bb:cc:dd:ee:ff", mac.formatted)
   }
 
   func testFormattedWithUpcasedMAC() {
     let mac = MACAddress("AA:bB:44:DD:ee:FF")
-    XCTAssertEqual("aa:bb:44:dd:ee:ff", mac.formatted())
+    XCTAssertEqual("aa:bb:44:dd:ee:ff", mac.formatted)
   }
 
   func testFormattedWithWhitespaceMAC() {
     let mac = MACAddress("  AA : bB:4 4:  DD:ee:FF  ")
-    XCTAssertEqual("aa:bb:44:dd:ee:ff", mac.formatted())
+    XCTAssertEqual("aa:bb:44:dd:ee:ff", mac.formatted)
   }
 
   func testFormattedWithoutColons() {
     let mac = MACAddress("aabb00ddee88")
-    XCTAssertEqual("aa:bb:00:dd:ee:88", mac.formatted())
+    XCTAssertEqual("aa:bb:00:dd:ee:88", mac.formatted)
+  }
+
+  func testFormattedWithInvalidMAC() {
+    let mac = MACAddress("aabb00ddeeXX")
+    XCTAssertEqual("aa:bb:00:dd:ee", mac.formatted)
+  }
+
+  func testhumanReadableWithInvalidMAC() {
+    let mac = MACAddress("aabb00ddeeXX")
+    XCTAssertEqual("??:??:??:??:??:??", mac.humanReadable)
+  }
+
+  func testIsValidWithValidMac() {
+    let mac = MACAddress("aabb00ddee8")
+    XCTAssertFalse(mac.isValid)
+  }
+
+  func testIsValidWithInvalidMac() {
+    let mac = MACAddress("aabb00ddee88")
+    XCTAssertTrue(mac.isValid)
+  }
+
+  func testEquatableWithTwoEqualAddresses() {
+    let mac1 = MACAddress("aa:Bb:cc:88:ee :ff")
+    let mac2 = MACAddress(" A A :bb:cc:88:ee:FF")
+    XCTAssertTrue(mac1 == mac2)
+  }
+
+  func testEquatableWithTwoDifferentAddresses() {
+    let mac1 = MACAddress("aa:aa:aa:aa:aa:aa")
+    let mac2 = MACAddress("bb:bb:bb:bb:bb:bb")
+    XCTAssertFalse(mac1 == mac2)
   }
 
 }
--- LinkTools/MACAdress.swift
+++ LinkTools/MACAdress.swift
@@ -1,6 +1,32 @@
 import Foundation
 
-class MACAddress {
+class MACAddress: Equatable {
+
+  var humanReadable: String {
+    get {
+      return isValid ? formatted : "??:??:??:??:??:??"
+    }
+  }
+
+  var formatted: String {
+    get {
+      return String(sanitized.characters.enumerated().map() {
+        $0.offset % 2 == 1 ? [$0.element] : [":", $0.element]
+        }.joined().dropFirst())
+    }
+  }
+
+  var isValid: Bool {
+    get {
+      return formatted.characters.count == 17
+    }
+  }
+
+  private var sanitized: String {
+    get {
+      return raw.lowercased().components(separatedBy: nonHexCharacters).joined()
+    }
+  }
 
   private var raw: String
   private lazy var nonHexCharacters = CharacterSet(charactersIn: "0123456789abcdef").inverted
@@ -9,14 +35,8 @@ class MACAddress {
     self.raw = raw
   }
 
-  func formatted() -> String {
-    return String(sanitized().characters.enumerated().map() {
-      $0.offset % 2 == 1 ? [$0.element] : [":", $0.element]
-    }.joined().dropFirst())
-  }
+}
 
-  private func sanitized() -> String {
-    return raw.lowercased().components(separatedBy: nonHexCharacters).joined()
-  }
-
+func ==(lhs: MACAddress, rhs: MACAddress) -> Bool {
+  return lhs.formatted == rhs.formatted
 }
