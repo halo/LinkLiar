6a833a92c2fd2bef72e8551e9a6cc577505ace9a
halo <github@posteo.org>
2022-04-25 20:39:08 +0200

    Re-connect to same SSID after diassociation

--- linkdaemon/Classes/Synchronizer.swift
+++ linkdaemon/Classes/Synchronizer.swift
@@ -115,14 +115,32 @@ class Synchronizer {
       return
     }
 
-    Log.info("Disassociating current Wi-Fi connection...")
-    CWWiFiClient.shared().interface()?.disassociate()
+    let ssid = CWWiFiClient.shared().interface()?.ssid()
+
+    if ssid == nil {
+      Log.info("You are not associated to any network.")
+    } else {
+      Log.info("Disassociating current Wi-Fi connection...")
+      CWWiFiClient.shared().interface()?.disassociate()
+    }
 
     Log.info("Setting MAC address <\(address.humanReadable)> for Interface \(BSDName)...")
     let task = Process()
     task.launchPath = "/sbin/ifconfig"
     task.arguments = [BSDName, "ether", address.formatted]
     task.launch()
+
+    if let ssid = ssid {
+      // Giving the Interface some time before attempting a re-connect to a network.
+      Log.info("Waiting for changes to take effect...")
+      sleep(1)
+      Log.info("Reconnecting to SSID <\(ssid)>")
+
+      let task = Process()
+      task.launchPath = "/usr/sbin/networksetup"
+      task.arguments = ["-setairportnetwork", BSDName, ssid]
+      task.launch()
+    }
   }
 
 }
