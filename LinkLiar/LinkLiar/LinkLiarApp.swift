8523db2f0f185c3dff369a32687574b2e2d4d9cb
halo <github@posteo.org>
2023-10-22 15:53:18 +0200

    Start from scratch 🤯

---
+++ LinkLiar/LinkLiar/LinkLiarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkLiarApp.swift
+//  LinkLiar
+//
+//  Created by orange on 22.10.23.
+//
+
+import SwiftUI
+
+@main
+struct LinkLiarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
